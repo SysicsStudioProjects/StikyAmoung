5417eee55619bfff3434e115f56181663856f015
Njim Hakim <njim.hakimnjim1992@gmail.com>
2021-01-23 17:07:27 +0100

    Kill event

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,17 +11,18 @@ public class PlayerMovement : MonoBehaviour
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
     public Animator anim;
-
+    bool ennemiisTarget;
     
     public float speedRotation;
     private void OnEnable()
     {
-        //MovementController.move += Move;
+        EventController.canKill += ChangeTarget;
+
     }
 
     private void OnDisable()
     {
-        
+        EventController.canKill -= ChangeTarget;
     }
 
     private void Start()
@@ -41,7 +42,7 @@ public class PlayerMovement : MonoBehaviour
         //mo= new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         Vector3 move = MovementController.Move();
         controller.Move(move * Time.deltaTime * playerSpeed);
-        print(move.z + move.x);
+
         anim.SetFloat("speed", Mathf.Abs(move.magnitude * Time.deltaTime * playerSpeed));
         if (move != Vector3.zero)
         {
@@ -61,14 +62,25 @@ public class PlayerMovement : MonoBehaviour
 
     void LockOnTarget(Vector3 _target)
     {
-        Quaternion startrotation = new Quaternion(0, 0, 0, 0);
-        // Vector3 dir = _target - transform.position;
-        // Quaternion lookRotation = Quaternion.LookRotation(dir);
-        // Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, 0.5f).eulerAngles;
-        //  LeanTween.rotate(BaseRotate.gameObject, new Vector3(0,dir.y,0), smoothtime);
-        // transform.rotation = Quaternion.Euler(0, rotation.y, 0);
-
-        Vector3 root = Vector3.Lerp(transform.forward, _target, speedRotation);
-        transform.forward = root;
+        if (ennemiisTarget==false)
+        {
+            Quaternion startrotation = new Quaternion(0, 0, 0, 0);
+            Vector3 root = Vector3.Lerp(transform.forward, _target, speedRotation);
+            transform.forward = root;
+        }
+      
     }
+
+        void ChangeTarget(Transform t)
+    {
+        if (t==null)
+        {
+            ennemiisTarget = false;
+        }
+        else
+        {
+            ennemiisTarget = true;
+        }
+    }
+    
 }
