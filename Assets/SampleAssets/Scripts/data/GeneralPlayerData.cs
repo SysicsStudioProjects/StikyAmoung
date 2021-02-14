99c990ae3b5a65d94a58c5157b94b4acc0b895e3
Njim Hakim <njim.hakimnjim1992@gmail.com>
2021-02-14 13:58:04 +0100

    Build Spine

--- Assets/SampleAssets/Scripts/data/GeneralPlayerData.cs
+++ Assets/SampleAssets/Scripts/data/GeneralPlayerData.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 [System.Serializable]
 public class GeneralPlayerData 
 {
     public int level;
     public int coins;
     public bool sound;
+    public int AdsSpinVideo;
+    public string spinDate;
+    public DateTime AdsspinDate;
     //public Skins skins;
     public List<int> state;
     public List<bool> toWatch;
@@ -18,6 +21,9 @@ public class GeneralPlayerData
         level = singleton.level;
         coins = singleton.coins;
         sound = singleton.sound;
+        AdsSpinVideo = singleton.AdsSpinVideo;
+        AdsspinDate = singleton.AdsspinDate;
+        spinDate = singleton.spinDate;
         // skins = singleton.skins;
         state = new List<int>();
         toWatch = new List<bool>();
